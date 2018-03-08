89caa525c1bc291b9fcc3f659ad13645fde54b7a
yangHui_To@163.com <yangHui_To@163.com>
2018-03-09 00:15:18 +0800

    2

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -36,6 +36,10 @@ public class HelloController {
 
     @PostMapping("/findByPage")
     public Map<String,Object> findByPage(@RequestParam Integer page, @RequestParam Integer rows){
+        Thread thread = new Thread(()->{
+            System.out.println("================");
+        });
+        thread.start();
         Map<String, Object> byPage = noticeService.findByPage(page, rows);
         return byPage;
     }
